62e482b8b9d87f436c638cf79c6ad8255f710257
Majiyd <majeed@patricia.com.ng>
2020-03-05 11:55:29 +0100

    create basic app view

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import Cameraroll from '@react-native-community/cameraroll';
-import {
-  SafeAreaView,
-  StyleSheet,
-  StatusBar,
-  TouchableOpacity,
-  Text,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, StatusBar, Text} from 'react-native';
 
 class App extends React.Component {
-  handleTakePhoto() {
-    console.log('take photo');
-  }
   render() {
     return (
       <>
         <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <TouchableOpacity onPress={this.handleTakePhoto}>
-            <Text>Take photo</Text>
-          </TouchableOpacity>
+        <SafeAreaView style={styles.container}>
+          <Text>Save Image</Text>
         </SafeAreaView>
       </>
     );
@@ -28,4 +16,11 @@ class App extends React.Component {
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2FF345CC',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
