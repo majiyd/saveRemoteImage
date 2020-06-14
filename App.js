1ce8cc4284ae5b144530b8c8de0acb0aa27f8e4e
Majiyd <majeed@patricia.com.ng>
2020-06-14 21:44:13 +0100

    Image downloader works

--- App.js
+++ App.js
@@ -8,37 +8,131 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
+  Platform,
+  PermissionsAndroid,
+  Alert,
+  ActivityIndicator,
 } from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
+import RNFetchBlob from 'rn-fetch-blob';
 
 class App extends React.Component {
   state = {
     url: '',
+    saving: false,
   };
 
   updateUrl = url => {
     this.setState({url});
   };
 
+  getPermissionAndroid = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        {
+          title: 'Image Download Permission',
+          message: 'Your permission is required to save images to your device',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        return true;
+      }
+      Alert.alert(
+        'Save remote Image',
+        'Grant Me Permission to save Image',
+        [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+        {cancelable: false},
+      );
+    } catch (err) {
+      Alert.alert(
+        'Save remote Image',
+        'Failed to save Image: ' + err.message,
+        [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+        {cancelable: false},
+      );
+    }
+  };
+
+  handleDownload = async () => {
+    // if device is android you have to ensure you have permission
+    if (Platform.OS === 'android') {
+      const granted = await this.getPermissionAndroid();
+      if (!granted) {
+        return;
+      }
+    }
+    this.setState({saving: true});
+    RNFetchBlob.config({
+      fileCache: true,
+      appendExt: 'png',
+    })
+      .fetch('GET', this.state.url)
+      .then(res => {
+        CameraRoll.saveToCameraRoll(res.data, 'photo')
+          .then(() => {
+            Alert.alert(
+              'Save remote Image',
+              'Image Saved Successfully',
+              [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+              {cancelable: false},
+            );
+          })
+          .catch(err => {
+            Alert.alert(
+              'Save remote Image',
+              'Failed to save Image: ' + err.message,
+              [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+              {cancelable: false},
+            );
+          })
+          .finally(() => this.setState({saving: false}));
+      })
+      .catch(error => {
+        this.setState({saving: false});
+        Alert.alert(
+          'Save remote Image',
+          'Failed to save Image: ' + error.message,
+          [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+          {cancelable: false},
+        );
+      });
+  };
+
   render() {
-    const {url} = this.state;
+    const {url, saving} = this.state;
     return (
       <>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView style={styles.container}>
           <View style={styles.app}>
-            <Text style={styles.headerText}>React Native Image Downloader</Text>
-            <View style={styles.textInputWrapper}>
-              <TextInput
-                placeholder="Enter image url here"
-                style={styles.textInput}
-                value={url}
-                onChangeText={text => this.updateUrl(text)}
-              />
-            </View>
-            <Image source={{uri: url}} style={styles.imagePreview} />
-            <TouchableOpacity style={styles.downloadButton}>
-              <Text>Download Image</Text>
-            </TouchableOpacity>
+            {saving ? (
+              <View style={styles.loader}>
+                <ActivityIndicator size="large" />
+              </View>
+            ) : (
+              <>
+                <Text style={styles.headerText}>
+                  React Native Image Downloader
+                </Text>
+                <View style={styles.textInputWrapper}>
+                  <TextInput
+                    placeholder="Enter image url here"
+                    style={styles.textInput}
+                    value={url}
+                    onChangeText={text => this.updateUrl(text)}
+                  />
+                </View>
+                <Image source={{uri: url}} style={styles.imagePreview} />
+                <TouchableOpacity
+                  style={styles.downloadButton}
+                  onPress={this.handleDownload}>
+                  <Text>Download Image</Text>
+                </TouchableOpacity>
+              </>
+            )}
           </View>
         </SafeAreaView>
       </>
@@ -91,5 +185,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 40,
     paddingVertical: 20,
     borderRadius: 3,
+  },
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
   },
 });
